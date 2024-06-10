cff26d0664c5a03f9de70b2e1a1a530a0b3ffe7c
Daniel Molina <danielmolina9202@gmail.com>
2024-06-10 13:43:00 -0400

    colores y css agregado

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,3 +16,19 @@ image.addEventListener("click",()=>{
 
 // PARTE DOS DEL DESAFÍO
 
+const stick1 = document.querySelector("#stick1")
+const stick2 = document.querySelector("#stick2")
+const stick3 = document.querySelector("#stick3")
+const button = document.querySelector(".btn")
+
+let resultado
+
+button.addEventListener("click",()=>{
+    console.log(stick1.value)
+    console.log(stick2.value)
+    console.log(stick3.value)
+    resultado = stick1.value + stick2.value + stick3.value
+    console.log(resultado)
+})
+
+
