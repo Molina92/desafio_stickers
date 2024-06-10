24908dfd1a5163dd0c0b2cf4db69159f1f5aac8f
Daniel Molina <danielmolina9202@gmail.com>
2024-06-10 17:33:33 -0400

    parte 2 en proceso

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,15 +20,28 @@ const stick1 = document.querySelector("#stick1")
 const stick2 = document.querySelector("#stick2")
 const stick3 = document.querySelector("#stick3")
 const button = document.querySelector(".btn")
+const parrafo = document.querySelector(".parrafo")
 
 let resultado
 
+
 button.addEventListener("click",()=>{
-    console.log(stick1.value)
-    console.log(stick2.value)
-    console.log(stick3.value)
-    resultado = stick1.value + stick2.value + stick3.value
+    resultado = parseInt(stick1.value) + parseInt(stick2.value) + parseInt(stick3.value)
+
+    if (resultado>10) {
+        parrafo.textContent += "LLEVAS DEMASIADOS STICKERS!"
+    } else {
+        parrafo.textContent += "LLEVAS " + resultado + " STICKERS!"
+    }
+
+    stick1.value = ""
+    stick2.value = ""
+    stick3.value = ""
+    console.log(parrafo.textContent)
     console.log(resultado)
+    parrafo.textContent=""
 })
+
+
 
 
