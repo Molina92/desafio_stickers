7b2fa99da2dc00a0e116186df1f4b7e982ead03f
Daniel Molina <danielmolina9202@gmail.com>
2024-06-10 11:10:01 -0400

    parte uno del desafio terminada

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,18 @@
-document.querySelector(".partOne")
+// PARTE UNO DEL DESAFÍO
+
+const image = document.querySelector(".image")
+const border = document.querySelector(".border")
+let bordeAgregado = false
+
+image.addEventListener("click",()=>{
+    if (!bordeAgregado) {
+        border.style.border = "2px solid red"
+        bordeAgregado= true
+    } else {
+        border.style.border = "none"
+        bordeAgregado= false
+    }
+})
+
+// PARTE DOS DEL DESAFÍO
+
