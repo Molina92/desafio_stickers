7c4552bdeedd98d4fc1d794f23497bde468a5ca6
Daniel Molina <danielmolina9202@gmail.com>
2024-06-11 09:07:01 -0400

    parte tres del desafio hecha

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,27 +21,37 @@ const stick2 = document.querySelector("#stick2")
 const stick3 = document.querySelector("#stick3")
 const button = document.querySelector(".btn")
 const parrafo = document.querySelector(".parrafo")
-
 let resultado
-
 
 button.addEventListener("click",()=>{
     resultado = parseInt(stick1.value) + parseInt(stick2.value) + parseInt(stick3.value)
 
     if (resultado>10) {
-        parrafo.textContent += "LLEVAS DEMASIADOS STICKERS!"
+        parrafo.textContent = "LLEVAS DEMASIADOS STICKERS!"
     } else {
-        parrafo.textContent += "LLEVAS " + resultado + " STICKERS!"
+        parrafo.textContent = "LLEVAS " + resultado + " STICKERS!"
     }
 
     stick1.value = ""
     stick2.value = ""
     stick3.value = ""
-    console.log(parrafo.textContent)
-    console.log(resultado)
-    parrafo.textContent=""
 })
 
+// PARTE TRES DEL DESAFÍO
 
+const buttonPassword = document.querySelector(".btn-password")
+const num1 = document.querySelector("#select1")
+const num2 = document.querySelector("#select2")
+const num3 = document.querySelector("#select3")
+const parrafo2 = document.querySelector(".parrafo2")
 
+buttonPassword.addEventListener("click", ()=> {
+    if (num1.value==='9' && num2.value==="1" && num3.value==="1") {
+        parrafo2.textContent = "Password 1 Correcto!"
+    } else if (num1.value==='7' && num2.value==="1" && num3.value==="4") {
+        parrafo2.textContent = "Password 2 Correcto!"
+    } else {
+        parrafo2.textContent = "Password Incorrecto!"
+    }
+})
 
